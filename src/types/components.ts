3e741cc6678bdb52a7b282ab290e319b034f9eb6
Nakarasan <krishzkara27@gmail.com>
2025-04-02 22:38:31 +0530

    add types for components

--- src/types/components.ts
+++ src/types/components.ts
@@ -4,3 +4,39 @@ export interface IButtonProps {
   type?: any;
   disabled?: boolean;
 }
+
+export interface InputProps {
+  name?: any;
+  type?: any;
+  label?: any;
+  onChange?: any;
+  values?: any;
+  autoFocus?: any;
+  onBlur?: any;
+  placeholder?: string;
+  onKeyDown?: any;
+  defaultValue?: any;
+  touches?: any;
+  errors?: any;
+  disabled?: boolean;
+  className?: string;
+  readOnly?: boolean;
+}
+
+export interface ITextareaProps {
+  name: string;
+  label?: string;
+  rows?: number;
+  onChange?: any;
+  values?: any;
+  errors?: any;
+  touches?: any;
+  onBlur?: any;
+  disabled?: boolean;
+  autoFocus?: any;
+  className?: any;
+  placeholder?: any;
+  onKeyDown?: any;
+  defaultValue?: any;
+  hidden?: any;
+}
