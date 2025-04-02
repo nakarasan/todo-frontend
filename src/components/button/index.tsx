b5c7ad9e44ba6d059d64f1b8ce40b8c484a02f56
Nakarasan <krishzkara27@gmail.com>
2025-04-02 23:21:52 +0530

    created reusable button component with props for customization

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -6,14 +6,29 @@ export const Button: React.FC<IButtonProps> = ({
   onClick,
   type,
   disabled,
+  variant = 'primary',
 }) => {
+  const getVariantStyles = (variant: IButtonProps['variant']) => {
+    switch (variant) {
+      case 'primary':
+        return 'py-2 px-4 bg-darkBlue text-white';
+
+      case 'secondary':
+        return 'py-2 px-4 border-2 border-darkBlue text-darkBlue';
+
+      default:
+        return null;
+    }
+  };
+  const variantStyles = getVariantStyles(variant);
   return (
     <button
       type={type}
+      className={`${variantStyles}  lg:px-6 text-p4 md:text-p2 tracking-widest font-bold rounded-md h-fit`}
       onClick={onClick}
       disabled={disabled}
     >
-      {name}
+      <span className='p-0'>{name}</span>
     </button>
   );
 };
