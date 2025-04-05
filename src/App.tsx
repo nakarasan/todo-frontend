831a5f9bc4250efdd18971f9928ad99aa0177846
Nakarasan <krishzkara27@gmail.com>
2025-04-05 18:49:46 +0530

    add toast message UI in

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,32 @@ import React from 'react';
 import Task from 'pages/task';
 import { Provider } from 'react-redux';
 import store from 'store/store';
+import { Toaster } from 'react-hot-toast';
 
-const App:React.FC =() => {
+const App: React.FC = () => {
   return (
     <Provider store={store}>
       <Task />
+      <Toaster
+        toastOptions={{
+          success: {
+            className:
+              'text-green-900 text-xs font-semibold tracking-widest bg-green-100 w-auto',
+            // icon: <SiTicktick className=' text-green-700  h-4 w-4' />,
+            position: 'top-right',
+            duration: 4000,
+          },
+          error: {
+            className:
+              'text-red-900 text-xs font-semibold tracking-widest bg-red-100 w-auto',
+            // icon: <RiErrorWarningLine className=' text-red-800  h-5 w-5' />,
+            position: 'top-right',
+            duration: 4000,
+          },
+        }}
+      />
     </Provider>
   );
-}
+};
 
 export default App;
